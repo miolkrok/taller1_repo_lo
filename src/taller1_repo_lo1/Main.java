8e35525058c350405ba1c02698a499b0ea262d96
Luis Ortiz <luchoaam97@gmail.com>
2022-05-17 19:53:35 -0500

    archivos necesarios para el proyecto

---
+++ src/taller1_repo_lo1/Main.java
@@ -0,0 +1,11 @@
+package taller1_repo_lo1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Mi primer proyecto JAVA en GitHub");
+
+	}
+
+}
